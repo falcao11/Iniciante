c9aa708380c7f8b86c6b71785703e2426942be58
falcao11 <goncalofonseca29@gmail.com>
2024-09-11 14:46:49 +0100

    fundamentos 004

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-//arrays,objetos
-
-let meta = {
-    value: "Ler um livro por mês",
-    //address: 2,
-    checked: false,
-    /*log: (info) =>{
-        console.log(info)
-    }*/
-}   //objeto
-
-meta.value = "não é mais ler um livro"
-//meta.log(meta.value)
-
-//function  // arrow function
-//const criarMeta = () => {}
-
-// function criarMeta() {}
-
-let metas = [
-    meta,
-    {
-        value: "Caminhar 2 minutos todos os dias",
-        checked: false
+function start () {
+    let count = 0;
+    while (count < 10) {
+        console.log(count)
+        count ++;
     }
-] //array
+}
 
-console.log(metas[0].value+ " e " + metas[1].value)
+start();
