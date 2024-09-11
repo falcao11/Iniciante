d4eb4f71798e52235036eede6a2544c03395ca6b
falcao11 <goncalofonseca29@gmail.com>
2024-09-11 14:35:46 +0100

    fundamentos 003

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 //arrays,objetos
 
-let metas = ["Ler um livro por mês", "Aprender inglês", "Fazer exercícios físicos"] //array
-
-console.log(metas[0]+ " e " + metas[1])
-
-
 let meta = {
     value: "Ler um livro por mês",
-    address: 2,
+    //address: 2,
     checked: false,
-    log: (info) => {
+    /*log: (info) =>{
         console.log(info)
-    }
+    }*/
 }   //objeto
 
 meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+//meta.log(meta.value)
 
 //function  // arrow function
 //const criarMeta = () => {}
 
 // function criarMeta() {}
+
+let metas = [
+    meta,
+    {
+        value: "Caminhar 2 minutos todos os dias",
+        checked: false
+    }
+] //array
+
+console.log(metas[0].value+ " e " + metas[1].value)
